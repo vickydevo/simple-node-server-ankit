08f91e58c8e62bfe85008697aa73f7e7405dfa71
vignan <vignan.kandela@gmail.com>
2023-03-06 21:03:25 +0530

    all files are added

---
+++ README.md
@@ -0,0 +1,7 @@
+# DOWNLOAD TO HOST SERVER
+use git clone
+change directory to index.js directory
+# npm install
+you will get package-lock.json and node_modules 
+then check for port  
+# node index.js
---
+++ index.js
@@ -0,0 +1,33 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    if (req.url == '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is the Default Page of the Server. If You have not specified anything or a Address, This is the page that is visible.</p></body></html>');
+        res.write('<html><body><p>  THIS IS CREATED IN WSL UBUNTU BY VIGNAN </p></body></html>');
+        res.end();
+
+    }
+    else if (req.url == "/home") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Home Page.</p></body></html>');
+        res.end();
+
+    }
+    else if (req.url == "/dashboard") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Dashboard Page.</p></body></html>');
+        res.end();
+    }
+    
+    else if (req.url == "/deploy") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is DEPLOYMENT page created by VIGNAN Page.</p></body></html>');
+        res.end();
+    }
+    else
+        res.end('Invalid Request!');
+});
+
+server.listen(3000);
+console.log('Node.js web server at port 3000 is running..')
